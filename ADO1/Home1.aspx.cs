19bbd2f5d2405164c642dd8b71d9685b60b247fe
bhaskarkum <b.k28011995@gmail.com>
2022-05-05 12:52:06 +0530

    ADO assignmemt1

---
+++ ADO1/Home1.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+namespace ADO1
+{
+    public partial class Home1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("data source =.; database =Master; integrated Security = SSPI");
+            SqlCommand cmd = new SqlCommand("select WinningTeam from FootBall WHERE MatchStatus ='WIN';SELECT * FROM FootBall WHERE TeamName1 ='JAPAN' OR TeamName2 ='JAPAN';  ", con);
+            con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            GridView1.DataSource = rdr;
+            GridView1.DataBind();
+
+            while (rdr.NextResult())
+            {
+                GridView2.DataSource = rdr;
+                GridView2.DataBind();
+            }
+            con.Close();
+
+        }
+
+    }
+}
